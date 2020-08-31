fa74ba87417e6c73f15154982a57301fceb4ee6c
Wonseok Song <dnjstjr0517@gmail.com>
2020-08-31 20:15:42 +0900

    Update: 프로젝트 초기화 express, pug 설치

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
 const app = express();
-const port = 5000
+const port = 8080;
+const route = require('./route.js');
 
-app.get("/", (req, res) => res.send("hello world"));
+app.set('view engine', 'pug'); // (1)
+app.set('views', path.join(__dirname, 'html')); // (2)
+app.use(express.static(path.join(__dirname, 'html')));
+
+app.use('/', route);
+// 에러 처리 부분
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
