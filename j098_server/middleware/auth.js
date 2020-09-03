621f726f1caaa48e1dd6bc4eb05a337ed3022e92
Wonseok Song <dnjstjr0517@gmail.com>
2020-09-04 02:26:05 +0900

    Feat: 검색기능 구현 - 1. 검색조건에 해당하는 숙소를 먼저 구한다. - 2. 검색조건의 체크인, 체크아웃 기간 내에 이미 예약된 내역이 있는지 확인 - 3. 예약된 애역이 없는 숙소만 결과로 보낸다.

--- j098_server/middleware/auth.js
+++ j098_server/middleware/auth.js
@@ -8,14 +8,11 @@ let auth = async (req, res, next) => {
     // sid가 있는지 확인
     // 있다면 해당하는 user가 존재하는지 확인
     const sid = req.cookies.sid;
-    console.log('sid: ' + sid);
-    console.log(sm.hashTable.has(sid));
     if (!sid || !sm.hashTable.has(sid))
         res.send('<script type="text/javascript"> alert("로그인해주세요"); window.location.href = "/login"</script>')
     else {
         const session = sm.get(sid);
         const user = await db.findOne('users', 'id', session.storage.id);
-        console.log(user);
         if (!user)
             res.send('<script type="text/javascript"> alert("로그인해주세요"); window.location.href = "/login"</script>')
     }
--- j098_server/routes/main.js
+++ j098_server/routes/main.js
@@ -3,15 +3,46 @@ const router = express.Router();
 
 const db = require('../database/MyDB')
 const DAY = 1000*60*60*24;
+const auth = require('../middleware/auth')
 
 
-router.post('/search', async (req, res, next) => {
-    res.render('signup');
+// 주소, 체크인, 체크아웃, 인원
+// address, check_in, check_out, personnel
+router.post('/search',async (req, res, next) => {
+    const { address, check_in, check_out, personnel} = req.body;
+
+    //1. 검색 주소와 인원에 맞는 숙소를 먼저 검색
+    const rooms = await db.find('rooms',['address','capacity'],['like','>='],[address,personnel]);
+
+    //2. 검색된 숙소중에서 해당 기간에 예약이 있는지 확인
+    const reserved = await db.find('reserved');
+
+    const checkIn = new Date(check_in).getTime();
+    const checkOut = new Date(check_out).getTime();
+
+    const searchResult = [];
+    rooms.forEach(room => {
+        const booking = reserved.filter(reservation => reservation.room_id == room.id);
+        for(let i=0;i<booking.length;i++){
+            const bookingCheckIn = new Date(booking[i].check_in).getTime();
+            const bookingCheckOut = new Date(booking[i].check_out).getTime();
+
+            // 검색한 체크인, 체크아웃 기간에 이미 예약이 있다면 return
+            if (checkIn <= bookingCheckIn && bookingCheckIn < checkOut){
+                return;
+            } else if (checkIn < bookingCheckOut && bookingCheckOut <= checkOut){
+                return;
+            }
+        }
+        searchResult.push(room);
+    })
+    
+    res.send(searchResult);
 })
 
 
 // 아직 테스트용
-router.post('/reserve', async (req, res, next) => {
+router.post('/reserve', auth,async (req, res, next) => {
     //비밀번호를 암호화 시킴
     const room = await db.findOne('rooms',req.body.room_id);
     const checkIn = new Date(req.body.check_in);
@@ -35,5 +66,10 @@ router.post('/reserve', async (req, res, next) => {
     res.send(reserves);
 
 })
+
+
+function availableReserve(){
+
+}
 
 module.exports = router;
