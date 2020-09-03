eb3d2d1b159dc1528968d62059bed1ff02e75c24
Wonseok Song <dnjstjr0517@gmail.com>
2020-09-04 00:51:52 +0900

    Update: 메인화면 수정

--- j098_server/routes/index.js
+++ j098_server/routes/index.js
@@ -9,16 +9,8 @@ const sm = require('../session/sessionManager')
 const db = require('../database/MyDB')
 
 router.get('/', async (req, res, next) => {
-  const data = await db.find('rooms', ['address'], ['like'], ['서울']);
-  let title = '';
-  let message = '';
-
-  if (data.length > 0) {
-    title = data[0].id;
-    message = data[0].name;
-  }
-  res.send(data);
-  // res.render('main', { title: title, message: message });
+ 
+  res.render('main', { title: title, message: message });
   
 });
 
