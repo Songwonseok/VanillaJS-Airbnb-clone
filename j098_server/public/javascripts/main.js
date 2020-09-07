f08f7cf70c5d4ec92b9523bcef940273cf5f144b
Wonseok Song <dnjstjr0517@gmail.com>
2020-09-08 01:56:24 +0900

    Feat: 검색창 검색오션 추가 - 요구사항 검색옵션을 추가하여 검색결과까지 확인

--- j098_server/public/javascripts/main.js
+++ j098_server/public/javascripts/main.js
@@ -1,5 +1,7 @@
 window.onload = () => {
     const menuBtnList = document.getElementsByClassName('tab_menu_btn');
+    const boxList = document.getElementsByClassName('tab_box');
+
     const stayBtn = document.getElementById('stayBtn');
     const experienceBtn = document.getElementById('experienceBtn');
     const onlineBtn = document.getElementById('onlineBtn');
@@ -12,15 +14,28 @@ window.onload = () => {
     }
 
     stayBtn.addEventListener('click', function(){
-        const context = document.getElementById('stay');
-        
+        const item = document.getElementById('stay');
+        removeOn(boxList);
+        item.classList.add('on');
+    })
+
+    experienceBtn.addEventListener('click', function () {
+        const item = document.getElementById('experience');
+        removeOn(boxList);
+        item.classList.add('on');
+    })
+
+    onlineBtn.addEventListener('click', function () {
+        const item = document.getElementById('online');
+        removeOn(boxList);
+        item.classList.add('on');
     })
 
 }
 
-function removeOn(menuBtnList){
-    for (var i = 0; i < menuBtnList.length; i++) {
-        menuBtnList[i].classList.remove('on')
+function removeOn(list){
+    for (var i = 0; i < list.length; i++) {
+        list[i].classList.remove('on')
     }    
 }
 
